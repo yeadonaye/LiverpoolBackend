f66a0cfc2647ed94e599dee6afac5cad6902c9c7
SENTAYEHU Yeadonaye Ashenafi <yeadonaye-ashenafi.sentayehu@etu.iut-tlse3.fr>
2026-03-26 21:58:39 +0100

    Remove unnecessary blank line in index.php

--- Controleur/afficher/index.php
+++ Controleur/afficher/index.php
@@ -5,7 +5,6 @@ require_once __DIR__ . '/../../Modele/Joueur.php';
 require_once __DIR__ . '/../../Modele/Match.php';
 require_once __DIR__ . '/../../Modele/DAO/connexionBD.php';
 
-
 $joueurDao = new JoueurDao($linkpdo);
 $matchDao = new MatchDao($linkpdo);
 
