50136b8ae0ccffe3c9fa8212c2c7228bc0fadb28
Nathan Tekeste <nathantek2006@gmail.com>
2026-03-28 10:39:29 +0100

    suppression de require once pour l auth.php

--- Controleur/afficher/afficher_joueur.php
+++ Controleur/afficher/afficher_joueur.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../Modele/DAO/auth.php';
+#require_once __DIR__ . '../../Modele/DAO/auth.php';
 require_once __DIR__ . '/../../Modele/DAO/JoueurDao.php';
 requireAuth();
 
